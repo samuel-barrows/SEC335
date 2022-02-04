47cdb1b865d3c98adbf521b771c2475afd7b2b8b
samuel-barrows <samuel.barrows@mymail.champlain.edu>
2022-02-04 10:49:25 -0500

    kali commit

--- portscan2.sh
+++ portscan2.sh
@@ -15,10 +15,43 @@ else
   speed=$3
 fi
 
-echo "FORMAT: host,port"
-while read -r host; do
-  while read -r port; do
-    timeout $speed bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
-      echo "FOUND: $host,$port"
-  done < $2
-done < $1
+host=$1
+port=$2
+
+fileext=$(echo $host | cut -d '.' -f 2)
+lastoct=$(echo $host | cut -d '.' -f 4)
+
+if [ $fileext = "txt" ]; then
+    echo "FORMAT: host,port"
+    while read -r host; do
+      while read -r port; do
+        timeout $speed bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
+        echo "FOUND: $host,$port"
+      done < $2
+    done < $1
+elif [ -z $lastoct ]; then
+    echo "scanning range..."
+    for addr in $(seq -f "$host.%g" 254); do
+        timeout $speed bash -c "echo >/dev/tcp/$addr/$port" 2>/dev/null &&
+	    echo "FOUND: $addr,$port"
+    done
+
+fi
+
+#if [ -z $lastoct ]; then
+#    echo "scanning range..."
+#    for addr in $(seq -f "$host.%g" 254); do
+#        timeout $speed bash -c "echo >/dev/tcp/$addr/$port" 2>/dev/null &&
+#	    echo "FOUND: $addr,$port"
+#    done
+#else
+#    echo "FORMAT: host,port"
+#    while read -r host; do
+#      while read -r port; do
+#        timeout $speed bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
+#        echo "FOUND: $host,$port"
+#      done < $2
+#    done < $1
+#fi
+
+
